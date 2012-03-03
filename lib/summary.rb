040187a8f5f6834751e0f6b0bdb159aeded4efa5
grafi <grafi@grafi.sakura.ne.jp>
2012-03-03 18:42:17 +0900

    fix indent

--- lib/summary.rb
+++ lib/summary.rb
@@ -3,6 +3,6 @@
 def make_summary(item, my_path = nil, option={})
   default_option = {created: true, updated: true, tag: true}
   option.merge default_option
-  @summary_template ||= ERB.new(File.read('layouts/summary_item.erb', encoding: 'UTF-8'), nil, '%')
+  @summary_template ||= ERB.new(File.read('layouts/summary_item.erb', encoding: 'UTF-8'), nil, '-')
   @summary_template.result(binding)
 end
